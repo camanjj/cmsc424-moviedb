635f8fecac4ec78d4349b3fda3eac80c49c25be5
Cameron Jackson <cameronjackson@Camerons-MBP-2.T-mobile.com>
2017-05-09 00:21:59 -0400

    feat: add category api methods

--- server.js
+++ server.js
@@ -5,30 +5,44 @@ var express = require('express')
 var app = express()
 var orm = require('orm');
 const _ = require('underscore')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json({type: 'application/*+json'}))
 
 app.use(orm.express(process.env.DATABASE_URL, {
   define: function (db, models, next) {
     models.keyword = db.define("keyword", {name: String})
+
+    models.category = db.define("category", {name: String})
+    models
+      .category
+      .hasOne('parent', models.category)
+
     models.dagr = db.define("dagr", {
-      gid: String,
+      id: {
+        type: 'text',
+        length: 32,
+        key: true
+      },
       file_name: String,
       file_path: String,
       file_type: String,
-      file_size: Int,
+      file_size: 'integer',
       file_alias: String,
       metadata: Object
     });
     models
       .dagr
       .hasOne('parent', models.dagr)
+    // models   .dagr   .hasMany('keywords', models.keyword)
     models
       .dagr
-      .hasMany('keywords', models.keyword)
+      .hasOne('category', models.category)
 
-    models.category = db.define("category", {name: String})
-    models
-      .category
-      .hasOne('parent', models.category)
+    // set the db object for raw queries
+    models.db = db
+
+    db.sync()
 
     next();
   }
@@ -38,9 +52,90 @@ app.use(orm.express(process.env.DATABASE_URL, {
 let port = (process.env.SERVER_PORT || process.env.PORT || 5000)
 
 app.post('/dagr', function (req, res) {
-  
+
   const dagr = _.pick(req.body, 'dagr')
   const keywords = _.pick(req.body, 'keywords')
+
+})
+
+app.get('/category', function (req, res) {
+
+  req
+    .models
+    .db
+    .driver
+    .execQuery('select * from category', (err, result) => {
+
+      if (err) {
+        res
+          .status(400)
+          .send(err);
+      } else {
+        res.send(result);
+      }
+
+    })
+
+})
+
+app.post('/category', (req, res) => {
+
+  req
+    .models
+    .category
+    .create(req.body, (err, result) => {
+      if (err) {
+        res.send(400, err);
+      } else {
+        res.send(result)
+      }
+    })
+})
+
+app.get('/category/:id/dagr', (req, res) => {
+
+  req
+    .models
+    .db
+    .driver
+    .execQuery("select * from dagr where category_id = ?", [req.params.id], (err, result) => {
+      if (err) {
+        res
+          .status(400)
+          .send(err);
+      } else {
+        res.send(result);
+      }
+    })
+
+})
+
+app.post('/category/:id/dagr/:dagrId', (req, res) => {
+
+  req
+    .models
+    .dagr
+    .get(req.params.dagrId, (err, dagr) => {
+
+      if (err) {
+        res
+          .status(400)
+          .send(err);
+        return;
+      }
+
+      dagr.category_id = id;
+      dagr.save(err => {
+        if (err) {
+          res
+            .status(400)
+            .send(err)
+        } else {
+          res.send(dagr)
+        }
+      })
+
+    })
 
 })
 
